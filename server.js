545934d1a7dc83e02b8ea8b83bcab89c90be03f0
Mason <m_nia@outlook.com>
2024-05-29 21:38:40 +1000

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const router = require("./routes/routes");
+const controller = require("./controllers/controller");
+
+app.use(express.static(__dirname + "/public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/", router);
+
+async function startServer() {
+  await controller.initializeCollection();
+  app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+  });
+}
+
+startServer();
